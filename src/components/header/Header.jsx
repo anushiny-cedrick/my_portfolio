e7367e61a49d505c457eb4fc4f9d653fbb7397b2
anushiny-cedrick <anushinyc@gmail.com>
2024-08-20 02:56:49 +0100

    hero section added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div  className='w-full px-5 lg:px-8 xl:px-[8%] py-3  items-center'>
+    <div  className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-6'>
       <img src="" alt="" />
       <h3>Hi!  I'am called <span>Anushiny</span> Cedrick</h3> 
-      <h1>Cloud Engineer</h1>
+      <h1> I am a Cloud Engineer base in Cameroon</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt aliquid tenetur optio, molestiae quibusdam  </p>
     </div>
   )
 }
