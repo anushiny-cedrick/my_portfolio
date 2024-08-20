23b6e61573e44380a519ba5fc5830f220641ef33
anushiny-cedrick <anushinyc@gmail.com>
2024-08-20 03:12:57 +0100

    hero creation containing button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,9 +4,12 @@ const Header = () => {
   return (
     <div  className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-6'>
       <img src="" alt="" />
-      <h3>Hi!  I'am called <span>Anushiny</span> Cedrick</h3> 
-      <h1> I am a Cloud Engineer base in Cameroon</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt aliquid tenetur optio, molestiae quibusdam  </p>
+      <h3>Hi!  I'am called <span>Anushiny</span> Cedrick.</h3> 
+      <h1 className='text-2xl sm:text-6xl lg:text-[55px]'> I am a Cloud Engineer base in Cameroon.</h1>
+      <p className='max-w-2xl mx-auto'>An IT expert with a great passion in the feilds of Web-Development, Cloud, Machine learning, Deep Learnin, Data Analysis and Data Secienc.</p>
+      <div className='flex gap-8'>
+        <a href="#contact">Contact Me</a>
+      </div>
     </div>
   )
 }
